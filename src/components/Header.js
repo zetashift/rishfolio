78993d7f17779b20b75c2a41b3148847703f1c3f
zetashift <rishi2@laptop.localdomain>
2021-02-05 22:45:06 +0100

    Disable Headroom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Headroom from "react-headroom"
+
 import { AnchorLink } from "gatsby-plugin-anchor-links"
 import { AiOutlineGithub } from "@react-icons/all-files/ai/AiOutlineGithub"
 import { AiOutlineMail } from "@react-icons/all-files/ai/AiOutlineMail"
@@ -14,23 +14,21 @@ const Logo = () => (
 
 export default function Header() {
   return (
-    <Headroom>
-      <nav>
-        <Logo />
-        <div className="nav-links">
-          <AnchorLink to="#about" title="About" className="nav-link" />
-          <AnchorLink to="#projects" title="Projects" className="nav-link" />
-          <AnchorLink to="#contact" title="Contact" className="nav-link" />
-          <div className="nav-icons">
-            <a href="https://github.com/zetashift" className="nav-link-icon">
-              <AiOutlineGithub />
-            </a>
-            <a href="mailto:rskaraya@gmail.com" className="nav-link-icon">
-              <AiOutlineMail />
-            </a>
-          </div>
+    <nav>
+      <Logo />
+      <div className="nav-links">
+        <AnchorLink to="#about" title="About" className="nav-link" />
+        <AnchorLink to="#projects" title="Projects" className="nav-link" />
+        <AnchorLink to="#contact" title="Contact" className="nav-link" />
+        <div className="nav-icons">
+          <a href="https://github.com/zetashift" className="nav-link-icon">
+            <AiOutlineGithub />
+          </a>
+          <a href="mailto:rskaraya@gmail.com" className="nav-link-icon">
+            <AiOutlineMail />
+          </a>
         </div>
-      </nav>
-    </Headroom>
+      </div>
+    </nav>
   )
 }
