95de3bdae99b53a98c679c267bafc8a19984b671
zetashift <rishi2@laptop.localdomain>
2021-02-06 02:01:14 +0100

    More tag alignment

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,27 +13,28 @@ export default function Project({
     <div className="project-container">
       <img src={img} alt={imgAlt} width="100%" height="400px" />
       <h1 className="project-title">{projectName}</h1>
-      <p className="project-description">{projectDescription}</p>
-      <div className="project-tags">
-        {projectTags.map(tag => (
-          <h3 key={tag}>{tag}</h3>
-        ))}
-      </div>
-      <div className="project-links">
-        {githubURL ? (
-          <a href={githubURL} className="project-link">
-            Github
-          </a>
-        ) : (
-          <></>
-        )}
-        {siteURL ? (
-          <a href={siteURL} className="project-link">
-            Live Site
-          </a>
-        ) : (
-          <></>
-        )}
+      
+        <p className="project-description">{projectDescription}</p>
+        <div className="project-tags">
+          {projectTags.map(tag => (
+            <h3 key={tag}>{tag}</h3>
+          ))}
+        </div>
+        <div className="project-links">
+          {githubURL ? (
+            <a href={githubURL} className="project-link">
+              Github
+            </a>
+          ) : (
+            <></>
+          )}
+          {siteURL ? (
+            <a href={siteURL} className="project-link">
+              Live Site
+            </a>
+          ) : (
+            <></>
+          )}
       </div>
     </div>
   )
